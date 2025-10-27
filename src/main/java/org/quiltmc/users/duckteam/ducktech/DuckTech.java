9ff3ccb3a5b0a06fda1a55b8206f91b2770131b4
kongzhongtitian <yun_yun_ax666@outlook.com>
2025-10-28 05:40:37 +0800

    1.-1.6

--- src/main/java/org/quiltmc/users/duckteam/ducktech/DuckTech.java
+++ src/main/java/org/quiltmc/users/duckteam/ducktech/DuckTech.java
@@ -29,7 +29,7 @@ public class DuckTech {
         LOGGER.info("|     | |    | |       |l       |     |     |       |    |");
         LOGGER.info("|     | |    | |     / | l      |     |     |     / |    |");
         LOGGER.info("|----/  l----|  l----  |  l     |     |----  l----  |    |");
-        LOGGER.info("DuckTech Version:1.-1.x");
+        LOGGER.info("DuckTech Version:1.-1.6");
         DTCreativeTab.CREATIVE_TABS.register(modEventBus);
         DTSounds.SOUND_EVENTS.register(modEventBus);
         DTBlocks.BLOCKS.register(modEventBus);
