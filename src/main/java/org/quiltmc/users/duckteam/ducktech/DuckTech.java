809029307c4524b6a9d20b92b116ef0565792ce3
kongzhongtitian <yun_yun_ax666@outlook.com>
2026-01-02 07:35:01 +0800

    add 自动替换系统

--- src/main/java/org/quiltmc/users/duckteam/ducktech/DuckTech.java
+++ src/main/java/org/quiltmc/users/duckteam/ducktech/DuckTech.java
@@ -33,7 +33,7 @@ public class DuckTech {
         LOGGER.info("|     | |    | |       |l       |     |     |       |    |");
         LOGGER.info("|     | |    | |     / | l      |     |     |     / |    |");
         LOGGER.info("|----/  l----|  l----  |  l     |     |----  l----  |    |");
-        LOGGER.info("DuckTech Version:1.-1.9");
+        LOGGER.info("DuckTech Version:1.-1.10");
 
         DTCreativeTab.CREATIVE_TABS.register(modEventBus);
         DTSounds.SOUND_EVENTS.register(modEventBus);
